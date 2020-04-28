311b0f442c353e35cf909a8308233531e852c37a
cofimfintech <63895109+cofimfintech@users.noreply.github.com>
2020-04-28 01:49:02 -0500

    04292020

---
+++ Cofim.App/Cofim.App/Views/LoginPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Cofim.App.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
